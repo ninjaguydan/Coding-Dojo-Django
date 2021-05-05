b31574280d7c673fde61b072bc48e698d3c1b067
Daniel Thompson <dthompson0190@gmail.com>
2021-05-04 19:28:49 -0500

    added personal profiles and CRUD functionality to books in project 14

--- 14_favbooks/favorite_books/fave_app/urls.py
+++ 14_favbooks/favorite_books/fave_app/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path('', views.books),
     path('logout', views.logout),
     path('add', views.add),
+    path('<int:book_id>', views.display_book),
+    path('<int:book_id>/fav', views.favorite),
+    path('<int:book_id>/unfav', views.unfavorite),
+    path('<int:book_id>/update', views.update),
+    path('<int:book_id>/delete', views.delete),
+    path('<int:user_id>/profile', views.profile),
 ]
--- 14_favbooks/favorite_books/fave_app/views.py
+++ 14_favbooks/favorite_books/fave_app/views.py
@@ -27,3 +27,43 @@ def add(request):
     new_book = Book.objects.create(title = request.POST['title'], desc = request.POST['desc'], uploaded_by = user)
     user.books.add(new_book)
     return redirect('/books')
+
+def display_book(request, book_id):
+    context = {
+        "book" : Book.objects.get(id = book_id),
+        "user" : User.objects.get(id = request.session['userid'])
+    }
+    return render(request, "fav_books.html", context)
+
+def favorite(request, book_id):
+    user = User.objects.get(id = request.session['userid'])
+    book = Book.objects.get(id = book_id)
+    user.books.add(book)
+    return redirect(f'/books/{book_id}')
+
+def unfavorite(request, book_id):
+    user = User.objects.get(id = request.session['userid'])
+    book = Book.objects.get(id = book_id)
+    user.books.remove(book)
+    return redirect(f'/books/{book_id}')
+
+def update(request, book_id):
+    if request.method == "GET":
+        return redirect(f'/books/{book_id}')
+    book = Book.objects.get(id = book_id)
+    book.title = request.POST['title']
+    book.desc = request.POST['desc']
+    book.save()
+    return redirect('/books')
+
+def delete(request, book_id):
+    book = Book.objects.get(id = book_id)
+    book.delete()
+    return redirect('/books')
+
+def profile(request, user_id):
+    context = {
+        "profile" : User.objects.get(id = user_id),
+        "user" : User.objects.get(id = request.session['userid'])
+    }
+    return render(request, "user.html", context)
