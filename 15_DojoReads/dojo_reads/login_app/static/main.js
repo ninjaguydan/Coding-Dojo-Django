b9d98afb9e756593556e510004ed0d28e2b17e99
Daniel Thompson <dthompson0190@gmail.com>
2021-05-11 23:07:46 -0500

    styled the remaining pages

--- 15_DojoReads/dojo_reads/login_app/static/main.js
+++ 15_DojoReads/dojo_reads/login_app/static/main.js
@@ -3,15 +3,26 @@ $('#sortable').sortable(
 );
 
 
-var rating = $('span').attr('rating');
-console.log(rating);
-var result = "";
-for (var i = 1; i < 6; i++) {
-    if (i <= rating) {
-        result = result + "&#x2605"
-    } else {
-        result = result  + "&#x2606";
-        // result += "&#x2605";
+//Display Star Rating
+function displayRating(num){
+    var result = "";
+    for (var i = 1; i < 6; i++) {
+        if (i <= num) {
+            result = result + "&#x2605"
+        } else {
+            result = result  + "&#x2606";
+            // result += "&#x2605";
+        }
     }
+    return result
 }
-$('span').html(result);
+
+// for (let x = 1; x < 6; X++) {
+//     $(".n"+x).html(displayRating(x))
+// }
+
+$('.n1').html(displayRating(1));
+$('.n2').html(displayRating(2));
+$('.n3').html(displayRating(3));
+$('.n4').html(displayRating(4));
+$('.n5').html(displayRating(5));
